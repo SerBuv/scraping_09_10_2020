8238c7b612452d21a60f8f88f645618527b13a61
SerBuv <sbuv62@mail.ru>
2020-10-19 23:01:20 +0300

    homework lesson 2

--- homeworks/les2/task1.py
+++ homeworks/les2/task1.py
@@ -20,7 +20,7 @@ from bs4 import BeautifulSoup as bs
 main_url = 'https://hh.ru'
 
 # position = input('Введите желаемую должность: ')
-position = 'Data engineer'
+position = 'Big Data'
 
 params = {
     'clusters': 'true',
@@ -30,50 +30,56 @@ params = {
     'L_save_area': 'true',
     'area': '1',
     'from': 'cluster_area',
-    'showClusters': 'true'
+    'showClusters': 'true',
+    'page': '0'
 }
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
 }
 
-
-response = requests.get(main_url + '/search/vacancy', params=params, headers=headers)
-soup = bs(response.text, 'html.parser')
-
-position_list = soup.findAll('div', {'class': 'vacancy-serp-item'})
-
 vacancies = []
-for vacancy in position_list:
-    vacancies_data = {}
-    vacancy_name = vacancy.find('span').getText()
-    vacancy_link = vacancy.find('a', {'class': 'bloko-link HH-LinkModifier'})['href']
-    vacancy_salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
-    if not vacancy_salary:
-        salary_min = None
-        salary_max = None
+while True:
+    response = requests.get(main_url + '/search/vacancy', params=params, headers=headers)
+    soup = bs(response.text, 'html.parser')
+
+    position_list = soup.findAll('div', {'class': 'vacancy-serp-item'})
+
+    for vacancy in position_list:
+        vacancies_data = {}
+        vacancy_name = vacancy.find('span').getText()
+        vacancy_link = vacancy.find('a', {'class': 'bloko-link HH-LinkModifier'})['href']
+        vacancy_salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+        if not vacancy_salary:
+            salary_min = None
+            salary_max = None
+        else:
+            vacancy_salary = vacancy_salary.getText()
+            if vacancy_salary.find('от') != -1:
+                vacancy_salary = vacancy_salary.split()
+                for i in vacancy_salary:
+                    salary_min = vacancy_salary[1] + vacancy_salary[2]
+            elif vacancy_salary.find('до') != -1:
+                vacancy_salary = vacancy_salary.split()
+                for i in vacancy_salary:
+                    salary_max = vacancy_salary[1] + vacancy_salary[2]
+            elif vacancy_salary.find('-') != -1:
+                vacancy_salary = vacancy_salary.split('-')
+                for i in vacancy_salary:
+                    salary_min = vacancy_salary[0].replace('\xa0', '')
+                    salary_max = vacancy_salary[1].replace('\xa0', '')
+                    salary_max = salary_max[:-5]
+
+        #print(1)
+        vacancies_data['name'] = vacancy_name
+        vacancies_data['link'] = vacancy_link
+        vacancies_data['salary_min'] = salary_min
+        vacancies_data['salary_max'] = salary_max
+
+        vacancies.append(vacancies_data)
+    next_link = soup.find('a', {'data-qa': ['pager-next']})
+    if next_link:
+        params['page'] = str(int(params['page']) + 1)
     else:
-        vacancy_salary = vacancy_salary.getText()
-        if vacancy_salary.find('от') != -1:
-            vacancy_salary = vacancy_salary.split()
-            for i in vacancy_salary:
-                salary_min = vacancy_salary[1] + vacancy_salary[2]
-        elif vacancy_salary.find('до') != -1:
-            vacancy_salary = vacancy_salary.split()
-            for i in vacancy_salary:
-                salary_max = vacancy_salary[1] + vacancy_salary[2]
-        elif vacancy_salary.find('-') != -1:
-            vacancy_salary = vacancy_salary.split('-')
-            for i in vacancy_salary:
-                salary_min = vacancy_salary[0].replace('\xa0', '')
-                salary_max = vacancy_salary[1].replace('\xa0', '')
-                salary_max = salary_max[:-5]
-
-    #print(1)
-    vacancies_data['name'] = vacancy_name
-    vacancies_data['link'] = vacancy_link
-    vacancies_data['salary_min'] = salary_min
-    vacancies_data['salary_max'] = salary_max
-
-    vacancies.append(vacancies_data)
+        break
 
 pprint(vacancies)
